08eb5c3750e6055c63ea6a3ffdf2b083348c1566
bkitano <briankitano@gmail.com>
2022-06-11 16:41:36 -0400

    adding title lol

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 const App = () => {
   return (
     <div className="app">
+      <h1>
+        Moire Stuff
+      </h1>
       <Link to="/circles">Circles</Link>
       <br />
       <Link to="/circle-wave">Circle Wave</Link>
