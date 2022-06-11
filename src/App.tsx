c9c8ed0a14d23a638b6f92c752683f7e0019dfd6
bkitano <briankitano@gmail.com>
2022-06-11 00:47:03 -0400

    figuring out viewbox

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,78 @@ const App = () => {
 
   return (
     <div className="App">
-      <svg
-        width={periods * scale_x + offset_x}
-        height={periods * scale_y + offset_y}
-      >
-        <Grid
+      {/* <svg width={periods * scale_x} height={periods * scale_y}>
+        <g offset={5}>
+          <Grid
+            {...{
+              end_x: periods * scale_x,
+              end_y: periods * scale_y,
+            }}
+          />
+          <Curve
+            {...{
+              offset_x: 0,
+              offset_y: 0,
+              period: scale_x,
+              amplitude: scale_y,
+            }}
+          />
+        </g>
+      </svg> */}
+      <svg width="1100px" height="1100px" viewBox="0 0 1100 1100">
+        <rect
           {...{
-            end_x: periods * scale_x + offset_x,
-            end_y: periods * scale_y + offset_y,
+            x: 0,
+            y: 550,
+            width: 530,
+            height: 530,
+            fill: "lightgreen",
+            stroke: "green",
+            strokeWidth: 20,
           }}
         />
-        <Curve
+        <rect
           {...{
-            offset_x,
-            offset_y,
-            period: scale_x,
-            amplitude: scale_y,
+            x: 550,
+            y: 550,
+            width: 530,
+            height: 530,
+            fill: "lightyellow",
+            stroke: "yellow",
+            strokeWidth: 20,
           }}
         />
+        <rect
+          {...{
+            x: 0,
+            y: 0,
+            width: 530,
+            height: 530,
+            fill: "lightblue",
+            stroke: "blue",
+            strokeWidth: 20,
+          }}
+        />
+        <rect
+          {...{
+            x: 550,
+            y: 0,
+            width: 530,
+            height: 530,
+            fill: "pink",
+            stroke: "red",
+            strokeWidth: 20,
+          }}
+        />
+        {/* <rect
+          {...{
+            x: 0,
+            y: 0,
+            width: 1100,
+            height: 1100,
+            fill: "red",
+          }}
+        /> */}
       </svg>
     </div>
   );
