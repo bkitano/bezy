7fde15e4ea612e0aee3952411c3de8a84f76e889
bkitano <briankitano@gmail.com>
2022-06-11 16:39:38 -0400

    adding home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 const App = () => {
-  return <div className="app">hi</div>;
+  return (
+    <div className="app">
+      <Link to="/circles">Circles</Link>
+      <br />
+      <Link to="/circle-wave">Circle Wave</Link>
+      <br />
+      <Link to="/waves">Waves</Link>
+    </div>
+  );
 };
 
 export default App;
